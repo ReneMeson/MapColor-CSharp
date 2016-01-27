88940261589e1cad7978d20396ea15c7bbb37dd2
praefectus <renemeson@gmail.com>
2016-01-27 20:33:58 -0200

    First Commit

---
+++ ColorMap/ColorMap/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ColorMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.panel3);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.panel2);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            resources.ApplyResources(this.trackBar1, "trackBar1");
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Value = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            this.panel2.DoubleClick += new System.EventHandler(this.panel2_DoubleClick);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Name = "panel3";
+            this.panel3.DoubleClick += new System.EventHandler(this.panel3_DoubleClick);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ColorMap/ColorMap/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorMap
+{
+    public partial class Form1 : Form
+    {
+        private SolidBrush myBrush;
+        private Graphics myGraphics;
+        private bool isDrawing = false;
+        //Bandera
+        private bool enablePaint = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myBrush = new SolidBrush(panel2.BackColor);
+            myGraphics = panel1.CreateGraphics();
+        }
+
+        private void panel2_DoubleClick(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                panel2.BackColor = colorDialog1.Color;
+                myBrush.Color = panel2.BackColor;
+
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing == true)
+            {
+                myGraphics.FillEllipse(myBrush, e.X, e.Y, trackBar1.Value, trackBar1.Value);
+            }
+        }
+
+        private void panel3_DoubleClick(object sender, EventArgs e)
+        {
+            //Backgroun canvas
+            if (colorDialog2.ShowDialog() == DialogResult.OK)
+            {
+                panel1.BackColor = colorDialog2.Color;
+                panel3.BackColor = colorDialog2.Color;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Incializa un componente SaveFileDialog.
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            //Cuando buscas archivos te muestra todos los .bmp.
+            saveFileDialog.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            //Titulo
+            saveFileDialog.Title = "Guardar gráfico como imagen";
+            // preguntamos si elegiste un nombre de archivo.
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Extención del archivo por defecto segun el filtro del saveFileDialog
+                switch (saveFileDialog.FilterIndex)
+                {
+                    case 1:
+                        saveFileDialog.DefaultExt = "jpg";
+                        break;
+
+                    case 2:
+                        saveFileDialog.DefaultExt = "bmp";
+                        break;
+
+                    case 3:
+                        saveFileDialog.DefaultExt = "gif";
+                        break;
+                }
+
+                //Obtenemos alto y ancho del panel
+                int width = panel1.Width;
+                int height = panel1.Height;
+                //Inicializamos un objeto BitMap con las dimensiones del Panel
+                Bitmap bitMap = new Bitmap(width, height);
+                //Inicializamos un objeto Rectangle en la posicion 0,0 y con dimensiones iguales a las del panel.
+                //0,0 y las mismas dimensiones del panel porque queremos tomar todo el panel
+                // o si solo queremos tomar una parte pues podemos dar un punto de inicio diferente y dimensiones distintas.
+                     Rectangle rec = new Rectangle(0, 0, width, height);
+                //Este metodo hace la magia de copiar las graficas a el objeto Bitmap
+                panel1.DrawToBitmap(bitMap, rec);
+                // Y por ultimo salvamos el archivo pasando como parametro el nombre que asignamos en el saveDialogFile
+                bitMap.Save(saveFileDialog.FileName);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!enablePaint)
+            {
+                isDrawing = true;
+            }
+            return;
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            enablePaint = true;
+        }
+    }
+}
